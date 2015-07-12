fc0acb38c75e631d86223ba841267991be874289
Steve Tickle <Steve@ZooKoo.com>
2015-07-12 08:47:21 +0100

    -

--- mkindex.c
+++ mkindex.c
@@ -4,6 +4,8 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 
 int main( int argc, char* argv[] )
@@ -14,6 +16,21 @@ int main( int argc, char* argv[] )
     fileHandle  = open( argv[1], O_RDONLY );
     if( fileHandle != -1 )
     {
+        ssize_t     offset              = 0;
+        uint8_t     block[1024*1024]    = {0};
+        ssize_t     bytesRead           = 0;
+
+        do
+        {
+            bytesRead           = read( fileHandle, &block[0], sizeof(block) );
+            if(bytesRead > 0)
+            {
+                printf( "bytesRead = %zd\n", bytesRead );
+            }
+            offset  += bytesRead;            
+
+        } while( bytesRead > 0 );
+
         close( fileHandle );
         exitCode    = 0;
     }
